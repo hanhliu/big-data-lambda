dc716a9b7fe7d5149ff04cab202b1e5991d2c8fa
hanhliu <hanh.hitachi.139@gmail.com>
2025-08-13 11:14:15 +0700

    first commit

---
+++ producer-springboot/src/test/java/org/example/producerspringboot/ProducerSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.producerspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProducerSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
